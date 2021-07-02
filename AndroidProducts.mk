d6ff1dd128243a1e09b3fd650ae0c71359f07e7a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-07-02 10:31:53 +0530

    lmi: Initial commit
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/descendant_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    descendant_lmi-user \
+    descendant_lmi-userdebug \
+    descendant_lmi-eng
